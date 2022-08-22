50e2e447299261e06a5bb148dc53d0a2177596e8
xinhua.pei <xinhua.pei@airudder.com>
2022-08-22 15:03:43 +0800

    update merge excel file function

--- ExcelOperations.py
+++ ExcelOperations.py
@@ -2,7 +2,7 @@
 Author: xinhua.pei xinhua.pei@airudder.com
 Date: 2022-08-18 11:30:17
 LastEditors: xinhua.pei xinhua.pei@airudder.com
-LastEditTime: 2022-08-18 11:30:37
+LastEditTime: 2022-08-22 14:59:53
 FilePath: /Python-tools/ExcelOperations.py
 Description: 
 
@@ -13,7 +13,7 @@ import pandas as pd
 import os
 import warnings
 
-merge_dire = '/Users/peixinhua/Downloads/merge'
+merge_dire = '/Users/peixinhua/Downloads/annotation file Indonesia'
 output_path = 'output.xlsx'
 df_list = []
 
@@ -23,21 +23,21 @@ remove_file_path = ''
 description: 合并excel 文件
 return {*} 合并为一整个的excel文案
 '''
-def merge_excel_file():
-    for f in os.listdir(merge_dire):
+def merge_excel_file(dire_path='', output_file_name= ''):
+    for f in os.listdir(dire_path):
         print('123131231{}'.format(f))
         if '.xlsx' in f:
             with warnings.catch_warnings(record= True):
                 warnings.simplefilter('always')
-                f_df = pd.read_excel(merge_dire + os.sep + f)
+                f_df = pd.read_excel(dire_path + os.sep + f)
                 f_df.insert(0,'file name',f)
                 print('f_df{}'.format(f_df))
                 df_list.append(f_df)
         
-    print('merge data{}'.format(merge_dire))
+    print('merge data{}'.format(dire_path))
 
     merde_df = pd.concat(df_list)
-    merde_df.to_excel(merge_dire + os.sep + output_path)
+    merde_df.to_excel(dire_path + os.sep + output_file_name)
 
 def remove_blank_row_or_column(input_file_path= '',output_file_path= '',row= False, column= False):
     if os.path.isfile(input_file_path):
@@ -51,6 +51,10 @@ def remove_blank_row_or_column(input_file_path= '',output_file_path= '',row= Fal
             df.to_excel(output_file_path)
 
 if __name__ == "__main__":
-    input_file_path = '/Users/peixinhua/Downloads/Action revoke case.csv'
-    output_file_path = '/Users/peixinhua/Desktop/output.xlsx'
-    remove_blank_row_or_column(input_file_path=input_file_path,output_file_path= output_file_path,row= True)
+    # input_file_path = '/Users/peixinhua/Downloads/Action revoke case.csv'
+    # output_file_path = '/Users/peixinhua/Desktop/output.xlsx'
+    # remove_blank_row_or_column(input_file_path=input_file_path,output_file_path= output_file_path,row= True)
+    # merge_excel_file()
+    dire_path = '/Users/peixinhua/Downloads/action revoke annotation file Indonesia'
+    output_file_name = 'output.xlsx'
+    merge_excel_file(dire_path=dire_path,output_file_name=output_file_name)
--- PandasUseCase.py
+++ PandasUseCase.py
@@ -2,7 +2,7 @@
 Author: xinhua.pei xinhua.pei@airudder.com
 Date: 2022-08-12 10:16:04
 LastEditors: xinhua.pei xinhua.pei@airudder.com
-LastEditTime: 2022-08-18 14:55:38
+LastEditTime: 2022-08-19 21:28:38
 FilePath: /Python-tools/PandasUseCase.py
 Description: 
 
@@ -23,33 +23,39 @@ df_list = []
 
 if not os.path.exists(output_path):
     os.makedirs(output_path)
+def concat_excel():
+    for path, dir_list, file_list in g:
+        for file_name in file_list:
+            if ".xlsx" in file_name:
+                # print('file name {}'.format(file_name))
+                df = pd.read_excel(os.path.join(path,file_name))
+                value = file_name.split(".")
+                df.insert(0,'file_name',value[0])
+                df.insert(0,'gender','Female')
+                df.insert(0,'value',df['File name'].str.split('.').str[0])
+                df_list.append(df)
 
-for path, dir_list, file_list in g:
-    for file_name in file_list:
-        if ".xlsx" in file_name:
-            # print('file name {}'.format(file_name))
-            df = pd.read_excel(os.path.join(path,file_name))
-            value = file_name.split(".")
-            df.insert(0,'file_name',value[0])
-            df.insert(0,'gender','Female')
-            df.insert(0,'value',df['File name'].str.split('.').str[0])
-            df_list.append(df)
+    merge_df = pd.concat(df_list)
+    merge_df.to_excel(os.path.join(output_path,'output1.xlsx'))
+    # print('merge_df {}'.format(merge_df))
+def mapping_excel():
+    # mapping_df: pd.DataFrame
+    # for path, dir_list, file_list in mapping_g:
+    #     for file_name in file_list:
+    #         if '.xlsx' in file_name:
+                # mapping_df = pd.read_excel(os.path.join(path,file_name))
+                merge_df = pd.read_excel('/Users/peixinhua/Downloads/annotation file Indonesia/output.xlsx')
+                temp_df = merge_df['File name'].str.split('.').str[0]
+                merge_df.insert(0,'value',temp_df)
+                mapping_df = pd.read_excel('/Users/peixinhua/Downloads/mapping/result (1).xlsx')
+                mapping_df['old_name'] = mapping_df['old_name'].str.split('/').str[-1]
+                mapping_df['new_name'] = mapping_df['new_name'].str.split('/').str[-1]
+                interval_df = mapping_df['old_name'].str.split('.').str[0].str[44:]
+                mapping_df.insert(3,'interval',interval_df)
+                mapping_df.insert(4,'value',mapping_df['new_name'].str.split('.').str[0])
+                # print('mapping_df {}'.format(mapping_df))
+                result = pd.merge(merge_df,mapping_df,how='left',on='value')
+                result.to_excel(os.path.join('/Users/peixinhua/Downloads/annotation file Indonesia','output2.xlsx'))
 
-merge_df = pd.concat(df_list)
-merge_df.to_excel(os.path.join(output_path,'output1.xlsx'))
-# print('merge_df {}'.format(merge_df))
-
-mapping_df: pd.DataFrame
-for path, dir_list, file_list in mapping_g:
-    for file_name in file_list:
-        if '.xlsx' in file_name:
-            mapping_df = pd.read_excel(os.path.join(path,file_name))
-            mapping_df['old_name'] = mapping_df['old_name'].str.split('/').str[-1]
-            mapping_df['new_name'] = mapping_df['new_name'].str.split('/').str[-1]
-            interval_df = mapping_df['old_name'].str.split('.').str[0].str[44:]
-            mapping_df.insert(3,'interval',interval_df)
-            mapping_df.insert(4,'value',mapping_df['new_name'].str.split('.').str[0])
-            # print('mapping_df {}'.format(mapping_df))
-result = pd.merge(merge_df,mapping_df,how='left',on='value')
-
-result.to_excel(os.path.join(output_path,'output2.xlsx'))
+if __name__ == '__main__':
+    mapping_excel()
