963120695124de371e638d0c213ca5504ffe8712
xinhua.pei <xinhua.pei@airudder.com>
2022-08-25 16:53:57 +0800

    show four plot

--- DialogueEffectCompare.py
+++ DialogueEffectCompare.py
@@ -2,7 +2,7 @@
 Author: xinhua.pei xinhua.pei@airudder.com
 Date: 2022-08-25 13:51:01
 LastEditors: xinhua.pei xinhua.pei@airudder.com
-LastEditTime: 2022-08-25 16:17:35
+LastEditTime: 2022-08-25 16:52:15
 FilePath: /Python-tools/DialogueEffectCompare.py
 Description: AIRudder 对话效果数据对比
 
@@ -10,10 +10,12 @@ Copyright (c) 2022 by xinhua.pei xinhua.pei@airudder.com, All Rights Reserved.
 '''
 
 import os
+from re import I
 from turtle import screensize
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import *
+# from matplotlib import *
 import pandas as pd
 
 data_dire_path = '/Users/peixinhua/Downloads'
@@ -22,6 +24,7 @@ robot_list_file_name = 'AB test robot list.xlsx'
 
 
 def read_excel_data():
+    i = 1
     robot_list_df = pd.read_excel(os.path.join(data_dire_path,robot_list_file_name),sheet_name='Sheet2')
     for row in robot_list_df.index:
         # print('row {}'.format(robot_list_df.loc[row]))
@@ -34,47 +37,57 @@ def read_excel_data():
                         test_type= test_type,
                         scenes= scenes
         )
+        i += 1
+        if i > 1:
+            return
+
 def get_robot_data(control_robot_id,test_robot_id,test_type,scenes):
     original_df = pd.read_excel(os.path.join(data_dire_path,file_name))
     filter_df = original_df[(original_df['robot_id']==control_robot_id) | (original_df['robot_id']==test_robot_id)]
-    print('filter_df {} \n'.format(filter_df))
+    draw_chart(show_df= filter_df)
+    # print('filter_df {} \n'.format(filter_df))
 
-def draw_chart():
-    fig, (ax1,ax2) = plt.subplots(2,1)
-    # make a little extra space between the subplots
-    fig.subplots_adjust(hspace=0.5)
+def draw_chart(show_df: pd.DataFrame):
+    print('filter_df {} \n'.format(show_df))
+    x_axis_data = show_df['day'][5:]
+    y_axis_daya = show_df['count']
+    plt.subplots_adjust(hspace=0.5)
+    # plot 1:
+    # xpoints = np.array([0,6])
+    # ypoints = np.array([0,100])
+    plt.subplot(2,2,1)
+    # plt.plot(xpoints,ypoints)
+    plt.title('plot 1')
+    plt.xticks(rotation=-60)
+    plt.plot(x_axis_data,y_axis_daya)
+    # plot 2:
+    # x = np.array([1,2,3,4])
+    # y = np.array([1,4,9,16])
+    plt.subplot(2,2,2)
+    # plt.plot(x,y)
+    plt.title('plot 2')
+    plt.xticks(rotation=-60)
+    plt.plot(x_axis_data,y_axis_daya)
 
-    dt = 0.01
-    t = np.arange(0,30,dt)
-    # Fixing random state for reproducibility
-    np.random.seed(19680801)
-    # white noise 1
-    nse1 = np.random.randn(len(t))
-    # white noise 1
-    nse2 = np.random.randn(len(t))
-    r = np.exp(-t / 0.05)
-    # colored noise 1
-    cnse1 = np.convolve(nse1,r,mode='same') * dt
-    # colored noise 2
-    cnse2 = np.convolve(nse2,r,mode='same') * dt
+    # plot 3:
+    plt.subplot(2,2,3)
+    plt.title('plot 3')
+    plt.xticks(rotation=-60)
+    plt.plot(x_axis_data,y_axis_daya)
 
-    # two signals with a coherent part and a random part
-    s1 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse1
-    s2 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse2
+    # plot 4:
+    plt.subplot(2,2,4)
+    plt.title('plot 4')
+    plt.xticks(rotation=-60)
+    plt.plot(x_axis_data,y_axis_daya)
 
-    ax1.plot(t,s1,t,s2)
-    ax1.set_xlim(0,5)
-    ax1.set_xlabel('time')
-    ax1.set_ylabel('s1 and s2')
-    ax1.grid(True)
-
-    cxy, f = ax2.csd(s1,s2,256,1. / dt)
-    ax2.set_ylabel('CSD (db)')
-
+    # overview
+    
+    plt.suptitle('RUNOOB subplot Test')
     plt.show()
 
 
 if __name__ == '__main__':
-    # read_excel_data()
-    draw_chart()
+    read_excel_data()
+    # draw_chart()
     
