c1a9b87ca7403c90c7c8f027de977d9e451dd67a
xinhua.pei <xinhua.pei@airudder.com>
2022-08-26 13:23:03 +0800

    draw char image complete

--- DialogueEffectCompare.py
+++ DialogueEffectCompare.py
@@ -2,7 +2,7 @@
 Author: xinhua.pei xinhua.pei@airudder.com
 Date: 2022-08-25 13:51:01
 LastEditors: xinhua.pei xinhua.pei@airudder.com
-LastEditTime: 2022-08-26 11:50:14
+LastEditTime: 2022-08-26 13:20:47
 FilePath: /Python-tools/DialogueEffectCompare.py
 Description: AIRudder 对话效果数据对比
 
@@ -66,13 +66,13 @@ def pretect_list_crash(values):
     #     value = '%.2f'%value
     return value
 
-def data_processing(show_df: pd.DataFrame,control_robot_id,test_robot_id):
+def data_processing(show_df: pd.DataFrame,list_titles,control_robot_id,test_robot_id):
     # data analysis
     day_df = show_df['day']
     day_drop_duplicates =  day_df.drop_duplicates(keep="first", inplace=False)
     day_values = day_drop_duplicates.values
     x_axis_data = list(dict.fromkeys(day_drop_duplicates.apply(convert_day_format).values))
-    list_titles = ['count','avgbillsec noFG','avgtalkround noFG','A']
+    # list_titles = ['count','avgbillsec noFG','avgtalkround noFG','A']
     return_dict = {}
     for title in list_titles:
         control_values = []
@@ -92,33 +92,30 @@ def data_processing(show_df: pd.DataFrame,control_robot_id,test_robot_id):
         
 
 def draw_chart(show_df: pd.DataFrame,control_robot_id,test_robot_id):
-    return_dict = data_processing(show_df=show_df,control_robot_id=control_robot_id,test_robot_id=test_robot_id)
+    list_titles = ['count','avgbillsec noFG','avgtalkround noFG','A','E']
+    return_dict = data_processing(show_df=show_df,list_titles=list_titles,control_robot_id=control_robot_id,test_robot_id=test_robot_id)
     x_axis_data = return_dict['day']
-    # list_titles = list(return_dict.keys())
-    list_titles = ['count','avgbillsec noFG','avgtalkround noFG','A']
-    # print('list titles {}'.format(list_titles))
     # the wodth of the bars
     width = 0.35
     x = np.arange(len(x_axis_data))
     plt.subplots_adjust(hspace=0.1)
+    control_label = 'Control Robot id:{}'.format(control_robot_id)
+    test_label = 'Test Robot id:{}'.format(test_robot_id)
     # plot 1:
-    plt.subplot(2,2,1)
-    plt.title(list_titles[0] + 'of call')
-    count_rects1 = plt.bar(x - width / 2,return_dict['count_control'],width=width,label=control_robot_id)
-    count_rects2 = plt.bar(x + width / 2,return_dict['count_test'],width=width,label=test_robot_id)
+    plt.subplot(3,2,1)
+    plt.title(list_titles[0] + ' of call')
+    count_rects1 = plt.bar(x - width / 2,return_dict['count_control'],width=width,label=control_label)
+    count_rects2 = plt.bar(x + width / 2,return_dict['count_test'],width=width,label=test_label)
     plt.legend()
     plt.bar_label(count_rects1,padding=3,rotation=90,fontsize= 6)
     plt.bar_label(count_rects2,padding=3,rotation=90,fontsize= 6)
     plt.xticks(ticks=x,labels=x_axis_data,rotation=60)
     plt.tight_layout()
     # plot 2:
-    plt.subplot(2,2,2)
+    plt.subplot(3,2,2)
     plt.title(list_titles[1])
-    # plt.xticks(rotation=60)
-    # figure_one_y_axis = show_df['count']
-    # plt.plot(x_axis_data,y_axis_daya)
-    avgbillsec_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgbillsec noFG_control']],width=width,label=control_robot_id)
-    avgbillsec_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgbillsec noFG_test']],width=width,label=test_robot_id)
+    avgbillsec_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgbillsec noFG_control']],width=width,label=control_label)
+    avgbillsec_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgbillsec noFG_test']],width=width,label=test_label)
     plt.legend()
     plt.bar_label(avgbillsec_rects1,padding=3,rotation=90,fontsize= 6)
     plt.bar_label(avgbillsec_rects2,padding=3,rotation=90,fontsize= 6)
@@ -126,13 +123,10 @@ def draw_chart(show_df: pd.DataFrame,control_robot_id,test_robot_id):
     plt.tight_layout()
 
     # plot 3:
-    plt.subplot(2,2,3)
+    plt.subplot(3,2,3)
     plt.title(list_titles[2])
-    # plt.xticks(rotation=60)
-    # figure_one_y_axis = show_df['count']
-    # plt.plot(x_axis_data,y_axis_daya)
-    avgtalkround_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgtalkround noFG_control']],width=width,label=control_robot_id)
-    avgtalkround_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgtalkround noFG_test']],width=width,label=test_robot_id)
+    avgtalkround_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgtalkround noFG_control']],width=width,label=control_label)
+    avgtalkround_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['avgtalkround noFG_test']],width=width,label=test_label)
     plt.legend()
     plt.bar_label(avgtalkround_rects1,padding=3,rotation=90,fontsize= 6)
     plt.bar_label(avgtalkround_rects2,padding=3,rotation=90,fontsize= 6)
@@ -140,13 +134,21 @@ def draw_chart(show_df: pd.DataFrame,control_robot_id,test_robot_id):
     plt.tight_layout()
 
     # plot 4:
-    plt.subplot(2,2,4)
-    plt.title(list_titles[3] + 'label rate')
-    # plt.xticks(rotation=60)
-    # figure_one_y_axis = show_df['count']
-    # plt.plot(x_axis_data,y_axis_daya)
-    A_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['A_control']],width=width,label=control_robot_id)
-    A_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['A_test']],width=width,label=test_robot_id)
+    plt.subplot(3,2,4)
+    plt.title(list_titles[3] + ' label rate')
+    A_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['A_control']],width=width,label=control_label)
+    A_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['A_test']],width=width,label=test_label)
+    plt.legend()
+    plt.bar_label(A_rects1,padding=3,rotation=90,fontsize= 6)
+    plt.bar_label(A_rects2,padding=3,rotation=90,fontsize= 6)
+    plt.xticks(ticks=x,labels=x_axis_data,rotation=60)
+    plt.tight_layout()
+
+    # plot 5:
+    plt.subplot(3,2,5)
+    plt.title(list_titles[4] + ' label rate')
+    A_rects1 = plt.bar(x - width / 2,[float('{:.2f}'.format(i)) for i in return_dict['E_control']],width=width,label=control_label)
+    A_rects2 = plt.bar(x + width / 2,[float('{:.2f}'.format(i)) for i in return_dict['E_test']],width=width,label=test_label)
     plt.legend()
     plt.bar_label(A_rects1,padding=3,rotation=90,fontsize= 6)
     plt.bar_label(A_rects2,padding=3,rotation=90,fontsize= 6)
