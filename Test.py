6abbe01cac822948cd1d4964e19cd3359d35f14f
xinhua.pei <xinhua.pei@airudder.com>
2022-09-09 17:16:43 +0800

    add test py file

--- Test.py
+++ Test.py
@@ -2,20 +2,30 @@
 Author: xinhua.pei xinhua.pei@airudder.com
 Date: 2022-08-10 19:48:20
 LastEditors: xinhua.pei xinhua.pei@airudder.com
-LastEditTime: 2022-08-12 12:18:35
+LastEditTime: 2022-09-05 15:18:40
 FilePath: /Python-tools/Test.py
 Description: 
 
 Copyright (c) 2022 by xinhua.pei xinhua.pei@airudder.com, All Rights Reserved. 
 '''
 # -*- coding: utf-8 -*
-# import matplotlib.pyplot as plt
-# plt.plot(range(10))
-# plt.title('哈哈',fontproperties="SimHei", fontsize=24)
-# plt.show()
-# print(plt.rcParams["font.family"])
-# print(plt.rcParams['font.sans-serif'])
-# print(plt.rc_params())
+# Declared using continuation character(\):
+s = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9
+print(s)
+# Declared using parentheses():
+n = (1 * 2 * 3 + 7 + 8 + 9)
+print(n)
+# Declared using square brackets[]:
+footballer = ['MESSI',
+            'NEYMAR',
+            'SUAREZ']
+print(footballer)
+# declared using braces {}:
+x = {1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9}
+print(x)
 
-var = 'String test'
-print(var[4:])
+# declared using semicolons(;):
+flag = 2; ropes = 3; pole = 4
+
