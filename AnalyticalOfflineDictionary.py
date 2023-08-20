71644bcafcf3b0d57ddd5c5ac4dd510556dcd280
xinhua.pei <xinhua.pei@airudder.com>
2023-08-20 09:46:24 +0800

    add example sentence

--- AnalyticalOfflineDictionary.py
+++ AnalyticalOfflineDictionary.py
@@ -2,32 +2,18 @@
 Author: xinhua.pei xinhua.pei@airudder.com
 Date: 2023-06-20 23:13:26
 LastEditors: xinhua.pei xinhua.pei@airudder.com
-LastEditTime: 2023-06-20 23:33:41
+LastEditTime: 2023-06-21 00:35:13
 FilePath: /Python-tools/AnalyticalOfflineDictionary.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-# from readmdict import MDX
-# mdx = MDX(os.path.join(root_path,file_name))
-# definition = mdx['hello']
-# print(definition)
-# print(mdx.name)
-# print(mdx.version)
+
 import os
 root_path = '/Users/peixinhua/Desktop/牛津高阶英汉双解词典(第9版)_v20191111'
 file_name = '牛津高阶英汉双解词典(第9版).mdx'
 
-# from readmdict import MDX
-# import pprint
-
-# # Create an instance of the MDX class and pass the path to your .mdx file
-# mdx = MDX(os.path.join(root_path,file_name))
-
-# # Get a list of all the words in the dictionary
-# words = mdx.keys()
-# print(words)
-
 from readmdict import MDX, MDD  # pip install readmdict
 from pyquery import PyQuery as pq    # pip install pyquery
+from bs4 import BeautifulSoup
 
 '''
 # 如果是windows环境，运行提示安装python-lzo，但
@@ -52,10 +38,37 @@ queryWord = 'hello'
 wordIndex = headwords.index(queryWord.encode())
 word,html = items[wordIndex]
 word,html = word.decode(), html.decode()
+# print(html)
 
-# 从html中提取需要的部分，这里以the litte dict字典为例。到这一步需要根据自己查询的字典html格式，自行调整了。
-# doc = pq(html)
+# 从html中提取需要的部分，这里以the little dict字典为例。到这一步需要根据自己查询的字典html格式，自行调整了。
+doc = pq(html)
+test = doc('sn-gs')
 # coca2 = doc('div[class="coca2"]').text().replace('\n','')
 # meaning = doc("""div[class="dcb"]""").text()
-# print(coca2)
-# print(meaning)
+# test = doc('div[class="licontent"]').text().replace('\n','')
+print(test.text().replace('\n',' '))
+
+
+
+
+# html = """
+# <html><head><title>The Dormouse's story</title></head>
+# <body>
+# <p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+# <p class="story">Once upon a time there were three little sisters; and their names were
+# <a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+# <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+# <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+# and they lived at the bottom of a well.</p>
+# <p class="story">...</p>
+# """
+
+# soup = BeautifulSoup(html, 'lxml')
+
+# print(soup.h-g eid="hello_hg_1".string)
+
+# print(soup.title)           # <title>The Dormouse's story</title>
+# print(type(soup.title))     # <class 'bs4.element.Tag'>
+# print(soup.title.string)    # The Dormouse's story
+# print(soup.head)            # <head><title>The Dormouse's story</title></head>
+# print(soup.p)               # <p class="title" name="dromouse"><b>The Dormouse's story</b></p>
